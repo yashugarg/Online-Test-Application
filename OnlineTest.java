10016e82e04b821ea6cf14eed7aa4867ef9715d2
Yashu Garg <garg.y2001@gmail.com>
2020-05-05 17:49:37 +0530

    Randomly choose Questions

--- OnlineTest.java
+++ OnlineTest.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -7,13 +6,64 @@ class OnlineTest extends JFrame implements ActionListener {
     JRadioButton but[] = new JRadioButton[5];
     JButton b1, b2;
     ButtonGroup bg;
-    int count = 0, current = 0, x = 1, y = 1, now = 0;
+    int marks = 0, current = 0, x = 1, y = 1, now = 0;
     int m[] = new int[10];
+    int[] quesIndex = new int[10];
+    public static String allQues[][] = {
+            { "Which one among these is not a keyword?", "class", "int", "get", "if", "get" },
+            { "Which one among these is not a primitive datatype?", "int", "Float", "boolean", "char", "Float" },
+            { "A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+                    "Encapsulation", "Polymorphism", "Abstraction", "Inheritance", "Abstraction" },
+            { "Which statement is true regarding an object?", "An object is what classes instantiated are from",
+                    "An object is an instance of a class", " An object is a variable",
+                    "An object is a reference to an attribute", "An object is an instance of a class" },
+            { "In object-oriented programming, new classes can be defined by extending existing classes. This is an example of:",
+                    "Encapsulation", "Interface", "Composition", "Inheritance", "Inheritance" },
+            { "The wrapping up of data and functions into a single unit is called", "Encapsulation", "Abstraction",
+                    "Data Hiding", "Polymorphism", "Encapsulation" },
+            { "In object-oriented programming, new classes can be defined by extending existing classes. This is an example of:",
+                    "Encapsulation", "Interface", "Composition", "Inheritance", "Inheritance" },
+            { "Given a class named student, which of the following is a valid constructor declaration for the class?",
+                    "Student (student s) { }", "Student student ( ) { }", "private final student ( ) { }",
+                    "void student ( ) { }", "Student (student s) { }" },
+            { "Which of these field declarations are legal within the body of an interface?",
+                    "private final static int answer = 42 ", "public static int answer=42", "final static answer =42",
+                    "int answer", "public static int answer=42" },
+            { "A package is a collection of", "Classes", "Interfaces", "Editing tools", "Classes and interfaces",
+                    "Classes and interfaces" },
+            { "Basic Java language functions are stored in which of the following java package?", " java.lang",
+                    "java.io", " java.util", "java.awt", " java.lang" },
+            { "Which of the following is a member of the java.lang package?", "List", "Queue", "Math", "Process",
+                    "Queue" },
+            { "What is the fundamental unit of information of writer streams?", "Characters", "Bytes", "Files",
+                    "Records", "Characters" },
+            { "File class is included in which package?", "java.io", "java.lang", "java.awt", "java.net", "java.io" },
+            { "URL stands for", "Universal reader locator", "Universal reform loader", "Uniform resource loader",
+                    "Uniform resource locator", "Uniform resource locator" },
+            { "What is the return type of the method getID() defined in AWTEvent class?", "Int", "long", "Object",
+                    "Component", "Int" },
+            { "Which one of these is a valid method declaration?", "void method1", "void method2()",
+                    "void method3(void)", "method4()", "void method2()" },
+            { "Given a class named Book, which one of these is a valid constructor declaration for the class?",
+                    "Book(Book b) { }", "Book Book() { }", "private final Book() { }", "void Book() { }",
+                    "Book(Book b) { }" },
+            { "Which one of the following class definitions is a valid definition of a class that cannot be extended?",
+                    "class Link { }", "abstract class Link { }", "static class Link { }", "final class Link { }",
+                    "final class Link { }" },
+            { "How restrictive is the default accessibility compared to public, protected, and private accessibility?",
+                    "Less restrictive than public", "More restrictive than public, but less restrictive than protected",
+                    "More restrictive than protected, but less restrictive than private",
+                    "More restrictive than private",
+                    "More restrictive than protected, but less restrictive than private" } };
 
     OnlineTest(String s) {
         super(s);
         l = new JLabel();
         add(l);
+        for (int i = 0; i < 10; i++) {
+            int randomIndex = (int) (Math.random() * 20);
+            quesIndex[i] = randomIndex;
+        }
         bg = new ButtonGroup();
         for (int i = 0; i < 5; i++) {
             but[i] = new JRadioButton();
@@ -27,24 +77,24 @@ class OnlineTest extends JFrame implements ActionListener {
         add(b1);
         add(b2);
         set();
-        l.setBounds(30, 40, 450, 20);
-        but[0].setBounds(50, 80, 100, 20);
-        but[1].setBounds(50, 110, 100, 20);
-        but[2].setBounds(50, 140, 100, 20);
-        but[3].setBounds(50, 170, 100, 20);
+        l.setBounds(30, 40, 800, 20);
+        but[0].setBounds(50, 80, 500, 20);
+        but[1].setBounds(50, 110, 500, 20);
+        but[2].setBounds(50, 140, 350, 20);
+        but[3].setBounds(50, 170, 305, 20);
         b1.setBounds(100, 240, 100, 30);
         b2.setBounds(270, 240, 100, 30);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setLocation(250, 100);
         setVisible(true);
-        setSize(600, 350);
+        setSize(900, 350);
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == b1) {
             if (check())
-                count = count + 1;
+                marks = marks + 1;
             current++;
             set();
             if (current == 9) {
@@ -54,7 +104,7 @@ class OnlineTest extends JFrame implements ActionListener {
         }
         if (e.getActionCommand().equals("Bookmark")) {
             JButton bk = new JButton("Bookmark" + x);
-            bk.setBounds(480, 20 + 30 * x, 100, 30);
+            bk.setBounds(780, 40 + 30 * x, 100, 30);
             add(bk);
             bk.addActionListener(this);
             m[x] = current;
@@ -69,7 +119,7 @@ class OnlineTest extends JFrame implements ActionListener {
         for (int i = 0, y = 1; i < x; i++, y++) {
             if (e.getActionCommand().equals("Bookmark" + y)) {
                 if (check())
-                    count = count + 1;
+                    marks = marks + 1;
                 now = current;
                 current = m[y];
                 set();
@@ -80,111 +130,39 @@ class OnlineTest extends JFrame implements ActionListener {
 
         if (e.getActionCommand().equals("Result")) {
             if (check())
-                count = count + 1;
+                marks = marks + 1;
             current++;
-            JOptionPane.showMessageDialog(this, "correct ans=" + count);
+            JOptionPane.showMessageDialog(this, "correct ans=" + marks);
             System.exit(0);
         }
     }
 
     void set() {
         but[4].setSelected(true);
-        if (current == 0) {
-            l.setText("Que1: Which one among these is not a primitive datatype?");
-            but[0].setText("int");
-            but[1].setText("Float");
-            but[2].setText("boolean");
-            but[3].setText("char");
+        for (int i = 0; i < 10; i++) {
+            if (current == i) {
+                l.setText("Ques: " + allQues[quesIndex[i]][0]);
+                but[0].setText(allQues[quesIndex[i]][1]);
+                but[1].setText(allQues[quesIndex[i]][2]);
+                but[2].setText(allQues[quesIndex[i]][3]);
+                but[3].setText(allQues[quesIndex[i]][4]);
+            }
         }
-        if (current == 1) {
-            l.setText("Que2: Which class is available to all the class automatically?");
-            but[0].setText("Swing");
-            but[1].setText("Applet");
-            but[2].setText("Object");
-            but[3].setText("ActionEvent");
-        }
-        if (current == 2) {
-            l.setText("Que3: Which package is directly available to our class without importing it?");
-            but[0].setText("swing");
-            but[1].setText("applet");
-            but[2].setText("net");
-            but[3].setText("lang");
-        }
-        if (current == 3) {
-            l.setText("Que4: String class is defined in which package?");
-            but[0].setText("lang");
-            but[1].setText("Swing");
-            but[2].setText("Applet");
-            but[3].setText("awt");
-        }
-        if (current == 4) {
-            l.setText("Que5: Which institute is best for java coaching?");
-            but[0].setText("Utek");
-            but[1].setText("Aptech");
-            but[2].setText("SSS IT");
-            but[3].setText("jtek");
-        }
-        if (current == 5) {
-            l.setText("Que6: Which one among these is not a keyword?");
-            but[0].setText("class");
-            but[1].setText("int");
-            but[2].setText("get");
-            but[3].setText("if");
-        }
-        if (current == 6) {
-            l.setText("Que7: Which one among these is not a class? ");
-            but[0].setText("Swing");
-            but[1].setText("Actionperformed");
-            but[2].setText("ActionEvent");
-            but[3].setText("Button");
-        }
-        if (current == 7) {
-            l.setText("Que8: which one among these is not a function of Object class?");
-            but[0].setText("toString");
-            but[1].setText("finalize");
-            but[2].setText("equals");
-            but[3].setText("getDocumentBase");
-        }
-        if (current == 8) {
-            l.setText("Que9: which function is not present in Applet class?");
-            but[0].setText("init");
-            but[1].setText("main");
-            but[2].setText("start");
-            but[3].setText("destroy");
-        }
-        if (current == 9) {
-            l.setText("Que10: Which one among these is not a valid component?");
-            but[0].setText("JButton");
-            but[1].setText("JList");
-            but[2].setText("JButtonGroup");
-            but[3].setText("JTextArea");
-        }
-        l.setBounds(30, 40, 450, 20);
+        l.setBounds(30, 40, 800, 20);
         for (int i = 0, j = 0; i <= 90; i += 30, j++)
-            but[j].setBounds(50, 80 + i, 200, 20);
+            but[j].setBounds(50, 80 + i, 500, 20);
     }
 
     boolean check() {
-        if (current == 0)
-            return (but[1].isSelected());
-        if (current == 1)
-            return (but[2].isSelected());
-        if (current == 2)
-            return (but[3].isSelected());
-        if (current == 3)
-            return (but[0].isSelected());
-        if (current == 4)
-            return (but[2].isSelected());
-        if (current == 5)
-            return (but[2].isSelected());
-        if (current == 6)
-            return (but[1].isSelected());
-        if (current == 7)
-            return (but[3].isSelected());
-        if (current == 8)
-            return (but[1].isSelected());
-        if (current == 9)
-            return (but[2].isSelected());
+        for (int i = 0; i < 10; i++) {
+            if (current == i) {
+                for (int j = 0; j < 4; j++) {
+                    if (but[j].isSelected()) {
+                        return (but[j].getActionCommand().equals(allQues[quesIndex[i]][5]));
+                    }
+                }
+            }
+        }
         return false;
     }
 
